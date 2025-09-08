d5325fba05504e11e585adae5b6e0df7e22bb74c
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-09-09 02:14:41 +0800

    Add project files.

---
+++ PhotoSaver/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhotoSaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The process started at: {0}", DateTime.Now);
+            Console.WriteLine("running...");
+
+            // Create a Timer object that knows to call our TimerCallback
+            // method once every 200 milliseconds.
+            Timer t = new Timer(ReadFiles, null, 0, 300);
+
+            //ReadFiles(null);
+
+            // Wait for the user to hit <Enter>
+            Console.ReadLine();
+        }
+
+        private static void TimerCallback(Object o)
+        {
+            // Display the date/time when this method got called.
+            Console.WriteLine("In TimerCallback: " + DateTime.Now);
+            // Force a garbage collection to occur for this demo.
+            GC.Collect();
+        }
+
+        private static void ReadFiles(Object o)
+        {
+            try
+            {
+                var rootFTP = @System.Configuration.ConfigurationManager.AppSettings["rootFTP"];
+                var rootPhoto = @System.Configuration.ConfigurationManager.AppSettings["rootPhoto"];
+                string[] teidS = Directory.GetDirectories(rootFTP, "*", SearchOption.TopDirectoryOnly);
+                
+                foreach (string teid in teidS)
+                {
+                    string[] cidS = Directory.GetDirectories(teid, "*", SearchOption.TopDirectoryOnly);
+                    
+                    foreach (string cid in cidS)
+                    {
+                        string[] dates = Directory.GetDirectories(cid, "*", SearchOption.TopDirectoryOnly);
+
+                        foreach (string date in dates)
+                        {
+                            string[] img50 = Directory.GetDirectories(date, "*", SearchOption.TopDirectoryOnly);
+
+                            foreach (string img01 in img50)
+                            {
+                                string[] imgS = Directory.GetFiles(img01, "*.jpg", SearchOption.TopDirectoryOnly);
+
+                                foreach (var img in imgS)
+                                {
+                                    var createTime = File.GetCreationTime(img);
+
+                                    if (DateTime.Now.AddMinutes(-3) > createTime)
+                                    {
+                                        Int32 unixTimestamp = (Int32)(createTime.Subtract(new DateTime(1970, 1, 1, 6, 0, 0))).TotalSeconds;
+                                        var rel = img.Substring(rootFTP.Length + 1);
+                                        var relPath = img.Substring(rootFTP.Length);
+
+                                        string strTEID = rel.Split('\\')[0];
+                                        int nTime = unixTimestamp;
+                                        int nPhotoType = 2;
+                                        string strAbsolutePath = rootPhoto + relPath;
+                                        relPath = relPath.Replace("\\", "/");
+                                        string strRelativePath = relPath;
+                                        int nCameraID = 0;
+
+                                        string insert = @"INSERT INTO 
+                                [Table_Photo]([strTEID]
+                                ,[nTime]
+                                ,[nPhotoType]
+                                ,[strRelativePath]
+                                ,[strAbsolutePath]
+                                ,[nCameraID])
+                                VALUES('" + strTEID + @"'
+                                ,'" + nTime + @"'
+                                ,'" + nPhotoType + @"'
+                                ,'" + strRelativePath + @"'
+                                ,'" + strAbsolutePath + @"'
+                                ,'" + nCameraID + @"')";
+
+                                        var db = new Services.MsSqlService();
+                                        var isInsert = db.Set("", insert);
+                                        if (isInsert)
+                                        {
+                                            // Determine whether the directory exists.
+                                            if (!Directory.Exists(strAbsolutePath))
+                                            {
+                                                Directory.CreateDirectory(Path.GetDirectoryName(strAbsolutePath));
+                                            }
+
+                                            if (!File.Exists(strAbsolutePath))
+                                            {
+                                                File.Copy(img, strAbsolutePath);
+                                            }
+
+                                            File.Delete(img);
+                                        }
+
+                                    }
+                                }
+
+                            }
+                            
+                        }
+
+                        //Console.WriteLine("deleted: {0}", imgDel);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The process failed: {0}", ex.ToString());
+            }
+
+            GC.Collect();
+
+        }
+
+
+    }
+}
---
+++ PhotoSaver/Services/MsSqlService.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSaver.Services
+{
+    public class MsSqlService
+    {
+        string GPSDB = System.Configuration.ConfigurationManager.ConnectionStrings["GPSDB"].ConnectionString;
+
+        public DataTable Get(string connectionString, string query)
+        {
+            var conn = new SqlConnection(GPSDB);
+            var cmd = new SqlCommand(query, conn);
+            cmd.CommandTimeout = 30;
+            var da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            conn.Open();
+            var ds = new DataSet();
+            da.Fill(ds);
+            conn.Close();
+            return ds.Tables[0];
+        }
+
+        public bool Set(string connectionString, string nonQuery)
+        {
+            bool status = false;
+            using (SqlConnection conn = new SqlConnection(GPSDB))
+            {
+                conn.Open();
+
+                SqlCommand cmd = conn.CreateCommand();
+                SqlTransaction tran;
+
+                // Start a local transaction.
+                tran = conn.BeginTransaction("oneTran");
+
+                // Must assign both transaction object and connection
+                // to Command object for a pending local transaction
+                cmd.Connection = conn;
+                cmd.Transaction = tran;
+
+                try
+                {
+
+                    cmd.CommandText = nonQuery;
+                    int affected = cmd.ExecuteNonQuery();
+
+                    if (affected > 0)
+                    {
+                        // Attempt to commit the transaction.
+                        tran.Commit();
+                        //Console.WriteLine("Both records are written to database.");
+                        status = true;
+                    }
+                    else
+                    {
+                        tran.Rollback();
+                    }
+
+                }
+                catch //(Exception ex)
+                {
+                    //Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                    //Console.WriteLine("  Message: {0}", ex.Message);
+
+                    // Attempt to roll back the transaction.
+                    try
+                    {
+                        tran.Rollback();
+                    }
+                    catch (Exception ex2)
+                    {
+                        // This catch block will handle any errors that may have occurred
+                        // on the server that would cause the rollback to fail, such as
+                        // a closed connection.
+                        //Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+                        //Console.WriteLine("  Message: {0}", ex2.Message);
+
+                        throw ex2;
+                    }
+                }
+            }
+            return status;
+        }
+
+        public bool Set(string connectionString, string[] nonQueries)
+        {
+            bool status = false;
+            int nAffect = 0;
+            using (SqlConnection conn = new SqlConnection(GPSDB))
+            {
+                conn.Open();
+
+                SqlCommand cmd = conn.CreateCommand();
+                SqlTransaction tran;
+
+                // Start a local transaction.
+                tran = conn.BeginTransaction("oneTran");
+
+                // Must assign both transaction object and connection
+                // to Command object for a pending local transaction
+                cmd.Connection = conn;
+                cmd.Transaction = tran;
+
+                try
+                {
+                    if (nonQueries != null && nonQueries.Length > 0)
+                    {
+                        for (int i = 0; i < nonQueries.Length; i++)
+                        {
+                            cmd.CommandText = nonQueries[i];
+                            nAffect += cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    if (nAffect == nonQueries.Length)
+                    {
+                        // Attempt to commit the transaction.
+                        tran.Commit();
+                        //Console.WriteLine("Both records are written to database.");
+                        status = true;
+                    }
+                    else
+                    {
+                        tran.Rollback();
+                    }
+
+                }
+                catch //(Exception ex)
+                {
+                    //Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                    //Console.WriteLine("  Message: {0}", ex.Message);
+
+                    // Attempt to roll back the transaction.
+                    try
+                    {
+                        tran.Rollback();
+                    }
+                    catch (Exception ex2)
+                    {
+                        // This catch block will handle any errors that may have occurred
+                        // on the server that would cause the rollback to fail, such as
+                        // a closed connection.
+                        //Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+                        //Console.WriteLine("  Message: {0}", ex2.Message);
+
+                        throw ex2;
+                    }
+                }
+            }
+            return status;
+        }
+
+
+    }
+}
